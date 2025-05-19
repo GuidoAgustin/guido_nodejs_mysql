76e78e1bef715b7049f4bd495ebcb1764e8e9bb2
Guido Bianchi <guidobianchi321@gmail.com>
2025-05-19 20:56:11 -0300

    arreglamos un pequeño error de syntaxis

--- app/application/users/LoginUser.js
+++ app/application/users/LoginUser.js
@@ -20,7 +20,7 @@ class LoginUser {
       first_name: user.first_name,
       last_name: user.last_name,
       email: user.email,
-      role: user.rol,
+      rol: user.rol,
     };
 
     return {
